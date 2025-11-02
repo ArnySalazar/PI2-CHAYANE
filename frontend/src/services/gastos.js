f2003262f3f91b91d6ee5ce407da398c6a3745d8
ArnySalazar <xxteam24xx@gmail.com>
2025-11-02 17:31:11 -0500

    soluionado gastos

--- frontend/src/services/gastos.js
+++ frontend/src/services/gastos.js
@@ -2,7 +2,14 @@ import api from './api'
 
 export default {
   async getAll(params = {}) {
-    const response = await api.get('/gastos', { params })
+    // Limpiar parámetros vacíos
+    const cleanParams = Object.fromEntries(
+      Object.entries(params).filter(
+        ([_, value]) => value !== '' && value !== null && value !== undefined,
+      ),
+    )
+
+    const response = await api.get('/gastos', { params: cleanParams })
     return response.data
   },
 
